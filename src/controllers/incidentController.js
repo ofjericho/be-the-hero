9228b2e767db9ef8fbc137d57cc25b5045188a83
unknown <wo@telavita.com.br>
2020-03-25 16:00:31 -0300

    Criação do Join entre Incidents e Ongs no Controller Incidents Método Index

--- src/controllers/incidentController.js
+++ src/controllers/incidentController.js
@@ -11,9 +11,17 @@ module.exports = {
 
     //Paginação 
     const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
-      .select('*');
+      .select([
+        'incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf'
+      ]);
     
     //Envio para o Header o total de resgistros
     response.header('X-Total-Count', count['count(*)']);

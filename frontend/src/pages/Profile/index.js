1703125579f00cbfe27bbde5eaadcae9ccc6f5d7
unknown <wo@telavita.com.br>
2020-03-28 12:28:21 -0300

    Conexão a API CRUD Ongs e Casos

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,80 +1,90 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower } from 'react-icons/fi';
 import { FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg'
 
 export default function Profile() {
+  const [incidents, setIncidents]= useState([]);
+  const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
+  const history = useHistory();
+
+  // Utilizado para carregamento logo após entrarmos na pagina
+  useEffect(() => {
+    api.get('profile', {
+        headers: {
+          Authorization: ongId,
+        }}).then(response => {setIncidents(response.data);
+      })
+    },[ongId]);
+
+  // Função para Deletar caso
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      // utilizado para ao Deletarmos um caso seja 
+      // removido do array incidentes
+      setIncidents(incidents.filter(incident => incident.id !== id));
+
+    } catch {
+      alert('Erro ao deletar caso, tente novamente.')
+    }
+  }
+
+  //Função para Logout
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
 
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be the Hero" />
-  <span>Bem vinda, {ongName}</span>
+        <span>Bem vinda, {ongName}</span>
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button type="button">
+        <button onClick={handleLogout} type="button">
           <FiPower size={18} color="E02041" />
         </button>
       </header>
+
       <h1>Casos cadastrados</h1>
 
       <ul>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
 
-          <strong>DESCRIÇÃO</strong>
-          <p>Descrição teste</p>
+        {incidents.map(incident => (
 
-          <strong>VALOR</strong>
-          <p>R$ 100,00</p>
-          <button type="button" >
-            <FiTrash2 size="20px" color="a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
+          <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
 
-          <strong>DESCRIÇÃO</strong>
-          <p>Descrição teste</p>
+            <strong>DESCRIÇÃO</strong>
+            <p>{incident.description}</p>
 
-          <strong>VALOR</strong>
-          <p>R$ 100,00</p>
-          <button type="button" >
-            <FiTrash2 size="20px" color="a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
+            <strong>VALOR</strong>
 
-          <strong>DESCRIÇÃO</strong>
-          <p>Descrição teste</p>
+            <p>{Intl.NumberFormat('pt-BR',
+                    { style: 'currency', currency: 'BRL'}
+                ).format(incident.value)}
+            </p>
 
-          <strong>VALOR</strong>
-          <p>R$ 100,00</p>
-          <button type="button" >
-            <FiTrash2 size="20px" color="a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
+            <button onClick={() => handleDeleteIncident(incident.id)} type="button" >
+              <FiTrash2 size="20px" color="a8a8b3" />
+            </button>
+          </li>
 
-          <strong>DESCRIÇÃO</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR</strong>
-          <p>R$ 100,00</p>
-          <button type="button" >
-            <FiTrash2 size="20px" color="a8a8b3" />
-          </button>
-        </li>
+        ))}
       </ul>
     </div>    
 
